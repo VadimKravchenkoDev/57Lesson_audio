e907a8e04418865bcbae3579fd0a632e98d39a7e
Arhizmei <arhizmei@gmail.com>
2024-03-17 14:36:15 +0200

    1.1

--- app/src/main/java/com/kravchenkovadim/a57lesson_audio/MainActivity.java
+++ app/src/main/java/com/kravchenkovadim/a57lesson_audio/MainActivity.java
@@ -2,6 +2,7 @@ package com.kravchenkovadim.a57lesson_audio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.stuff);
+        mediaPlayer.start();
+
     }
 }
