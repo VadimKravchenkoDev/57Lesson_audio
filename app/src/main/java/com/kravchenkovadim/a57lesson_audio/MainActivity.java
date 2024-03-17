afd9d5050f0458fc7e446bfa73c9bd021ffc6ad5
Arhizmei <arhizmei@gmail.com>
2024-03-17 15:09:21 +0200

    1.3

--- app/src/main/java/com/kravchenkovadim/a57lesson_audio/MainActivity.java
+++ app/src/main/java/com/kravchenkovadim/a57lesson_audio/MainActivity.java
@@ -4,16 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    MediaPlayer mediaPlayer;
+    Button playButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+       playButton = findViewById(R.id.playButton);
+       mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.stuff);
+       playButton.setOnClickListener(       );
 
-        MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.stuff);
-        mediaPlayer.start();
+
 
     }
+
+    public void play(View view) {
+        mediaPlayer.start();
+        if (playButton.getText() == "Play"){
+            mediaPlayer.start();
+            playButton.setText("Pause");
+        } else {
+            mediaPlayer.pause();
+        }
+
+    }
+
+
 }
