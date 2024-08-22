da79261713baef40bb7c140df7b8a925e1bb1b34
Arhizmei <arhizmei@gmail.com>
2024-08-22 18:39:16 +0300

    changed color of buttom

--- app/src/main/java/com/kravchenkovadim/a57lesson_audio/MainActivity.java
+++ app/src/main/java/com/kravchenkovadim/a57lesson_audio/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
         audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
         int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
 
